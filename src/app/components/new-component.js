4a462ba57b6098e7cec600fd2bd97abf6ff9ae7f
Jesus Molina Jimenez <jmolinaj@vectoritcgroup.com>
2017-04-19 12:28:50 +0200

    a

--- src/app/components/new-component.js
+++ src/app/components/new-component.js
@@ -1,3 +1,19 @@
 angular.module('app').component('newComponent',{
-    templateUrl: 'app/components/newComponent-template.html'
-})
+    templateUrl: 'app/components/newComponent-template.html',
+    controller: newComponent,
+    controllerAs:'newComponent',
+    bindings:{
+        foo: '@'
+    }
+});
+
+function newComponent(){
+
+    var vm=this;
+
+    vm.$onInit = function(){
+        vm.miVariable = vm.foo;
+
+    }
+
+}
